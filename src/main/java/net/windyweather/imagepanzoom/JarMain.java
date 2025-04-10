5987f395c8e83eb56e7b8efbe6060f0b00387089
Windy Player <windyplayer@gmx.com>
2025-04-10 15:59:42 -0700

    Fix building a JAR file that runs.

---
+++ src/main/java/net/windyweather/imagepanzoom/JarMain.java
@@ -0,0 +1,9 @@
+package net.windyweather.imagepanzoom;
+
+import javafx.application.Application;
+
+public class JarMain {
+    public static void main(String[] args ) {
+        Application.launch(PanZoomImage.class, args);
+    }
+}
